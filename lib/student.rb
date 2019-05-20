fd4ee10c1601dac16ba19fc046f77f36d305eac9
Ricartho Franck <ricartho.franck@esih.edu>
2019-05-20 17:58:21 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -39,7 +39,8 @@ attr_reader :id
   end 
   
   def self.create(name: grade:)
-      Student.new(name,grade)
-      
+      student = Student.new(name,grade)
+      student.save
+      student
   end 
 end
