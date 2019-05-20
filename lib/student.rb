7b3be33a0c08ae991016cf53901fa30f61f1e24a
Ricartho Franck <ricartho.franck@esih.edu>
2019-05-20 17:51:54 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,6 +35,6 @@ attr_reader :id
       VALUES(?,?)
       SQL
     DB[:conn].execute(sql,self.name,self.grade)
-    @id= DB[:conn].execute(SELECT last_insert_row id() FROM students)[0][0]
+    @id= DB[:conn].execute(SELECT last_insert_rowid() FROM students)[0][0]
   end 
 end
